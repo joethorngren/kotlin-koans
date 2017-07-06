08e08ef7570cbb995c90fada73505d72033119fe
Joe Thorngren <joethorngren@gmail.com>
2017-07-06 08:14:12 -0600

    Starting conventions.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return if(this.year > other.year) {
+            1
+        } else if(this.month > other.month) {
+            1
+        } else {
+            0
+        }
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
